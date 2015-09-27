03e117eedd64a94ac237cbc14e0f43caa5a530f2
Dipen Chauhan <get.dipen@gmail.com>
2015-09-27 16:57:11 -0700

    Force users to login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # force the user to redirect to the login page if the user was not logged in
+  before_filter :authenticate_user!
 end
