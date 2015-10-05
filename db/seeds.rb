265c4d992de924632d8ba6c336ab8147f177d565
Dipen Chauhan <get.dipen@gmail.com>
2015-10-05 16:13:59 -0700

    Sample properties and users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+properties = Property.create(
+	[
+		{ name: 'Hilton Los Angeles Airport', address_line1: '5711 W Century Blvd', city: 'Los Angeles', state: 'CA', zip: '90045', phone: '(310) 410-4000'},
+		{ name: 'Americas Best Value Inn Chico', address_line1: '740 Broadway Street', city: 'Chico', state: 'CA', zip: '95926', phone: '(530) 343-3286'}
+	]
+)
+
+users = User.create(
+	[
+		{ email: 'a@a.com', password: 'a', password_confirmation: 'a', firstname: "", lastname: "", property_id: properties[0]},
+		{ email: 'b@b.com', password: 'b', password_confirmation: 'b', firstname: "", lastname: "", property_id: properties[1]},
+		{ email: 'c@c.com', password: 'c', password_confirmation: 'c', firstname: "", lastname: "", property_id: properties[0]}
+	]
+)
+
