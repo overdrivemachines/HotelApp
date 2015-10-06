f08db16adda944899435ac103b882893c3318d0d
Dipen Chauhan <get.dipen@gmail.com>
2015-10-05 17:03:56 -0700

    Made status into an enum

--- app/models/room.rb
+++ app/models/room.rb
@@ -18,7 +18,9 @@
 #
 
 class Room < ActiveRecord::Base
-  belongs_to :property
-  belongs_to :room_type
-  has_many :reservations
+	belongs_to :property
+	belongs_to :room_type
+	has_many :reservations
+
+	enum status: [:clean, :dirty, :out_of_order]
 end
