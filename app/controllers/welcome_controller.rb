5c169330aa60fd8d09d7a674ed8f34c871fb6223
Dipen Chauhan <get.dipen@gmail.com>
2015-10-13 11:20:03 -0700

    Set current users property

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,25 @@
 class WelcomeController < ApplicationController
-  def index
-  end
+	def index
+	end
+
+	def update
+		# Find the property using property_id from params
+		p = Property.where(id: params[:user][:property_id]).first
+		# Check if property exists
+		if p
+			# Property exists
+
+			# Set the current user's property
+			current_user.property_id = p.id
+			current_user.save
+
+			# current_user.update(params.require(:user).permit(:firstname, :property_id))
+
+			redirect_to root_url, notice: 'Successfully enrolled in property ' + current_user.property_id.to_s 
+
+		else
+			# Property does not exist
+			redirect_to root_url, notice: 'Property with id ' + params[:user][:property_id] + ' not found'
+		end
+	end
 end
