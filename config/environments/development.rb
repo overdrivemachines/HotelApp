1f4f12022939b2c79e7965a2263ec66194d18dec
Dipen Chauhan <get.dipen@gmail.com>
2015-10-27 17:02:44 -0700

    Hide SQL queries in log

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,5 +39,8 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
+  # Hide SQL queries
+  config.log_level = :info
+
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
