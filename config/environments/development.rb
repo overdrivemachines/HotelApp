ec136a57ca8ccb3a4e365e66821be654b2aa0086
Dipen Chauhan <get.dipen@gmail.com>
2015-12-13 15:23:52 -0800

    Added mailer smtp settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,5 +42,14 @@ Rails.application.configure do
   # Hide SQL queries
   config.log_level = :info
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+   :address              => "smtp.gmail.com",
+   :port                 => 587,
+   :user_name            => ENV['gmail_username'],
+   :password             => ENV['gmail_password'],
+   :authentication       => "plain",
+  :enable_starttls_auto => true
+  }
 end
