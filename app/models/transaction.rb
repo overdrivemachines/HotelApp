83e11ddaeb7207e89876427e1372c627b9ae6e6e
Dipen Chauhan <get.dipen@gmail.com>
2015-12-13 15:30:42 -0800

    Added enum type

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -6,6 +6,7 @@
 #  reservation_id :integer
 #  description    :string
 #  amount         :decimal(8, 2)
+#  type           :integer
 #  created_at     :datetime         not null
 #  updated_at     :datetime         not null
 #
@@ -16,4 +17,13 @@
 
 class Transaction < ActiveRecord::Base
   belongs_to :reservation
+
+  enum type: {charge_room: 0,
+  	charge_custom: 1, 
+  	credit_cc: 2,
+  	credit_cash: 3,
+  	credit_custom: 4
+  }
+
+  # charge_custom and credit_custom are entered by user
 end
